7eae8daa70289f278f3d30a58d66760abf08cdf1
Adam Jones <adam@ayeteejay.com>
2021-07-07 13:37:08 -0700

    Adding in buttons

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Sphere from '../images/sphere-logo.png';
 
 const Hero = () =>{
     return (
         <div className="hero">
+            <img src={Sphere} className="logo"/>
             <h1>Sphere Component Library</h1>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,31 @@ import * as React from "react";
 import "../styles/styles.scss";
 import Layout from "../components/layout";
 import Hero from "../components/hero";
+import Helmet from "react-helmet";
 
 const Index = () => {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sphere</title>
+      </Helmet>
+      <Hero></Hero>
       <div className="content">
-        <Hero></Hero>
+        <div className="row">
+          <h2>Buttons</h2>
+          <div className="button-row">
+            <a href="#" className="primary-btn">
+              Primary Button
+            </a>
+            <a href="#" className="alternative-btn">
+              Alternative Button
+            </a>
+            <a href="#" className="secondary-btn">
+              Secondary Button
+            </a>
+          </div>
+        </div>
       </div>
     </Layout>
   );
